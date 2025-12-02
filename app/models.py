738a27105fbecce171eb8b43a619b50d14fd2d4c
Coelho <dcoelho.me@gmail.com>
2025-12-01 22:27:27 -0300

    its working apparently

--- app/models.py
+++ app/models.py
@@ -7,10 +7,12 @@ class Logs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     log = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
 
     def to_dict(self):
         return {
             "id": self.id,
             "log": self.log,
-            "author": self.author
+            "author": self.author,
+            "created_at": self.created_at.isoformat() if self.created_at else None
         }
